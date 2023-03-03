de04b78e9d811d45f66bded02ec8fc200083026a
Willian Marioti <willian.marioti@gmail.com>
2023-03-03 14:05:27 -0300

    Add project files.

---
+++ Fibonacci/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Diagnostics;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        double ant = 1, prox = 0;
+        int posicao;
+
+        Console.WriteLine("Informe a posição: ");
+        posicao = int.Parse(Console.ReadLine());
+
+        for(int i = 0; i < posicao; i++)
+        {
+            Console.Write(ant + prox + " ");
+            
+            prox = ant + prox;
+            ant = prox - ant;
+
+        }
+
+    }
+}
