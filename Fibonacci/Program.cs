632b6836454c4e16167655730c7394446ece862c
Willian Marioti <willian.marioti@gmail.com>
2023-03-05 13:08:07 -0300

    Foi utilizado vetor para chegar ao mesmo resultado.

--- Fibonacci/Program.cs
+++ Fibonacci/Program.cs
@@ -4,20 +4,43 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        double ant = 1, prox = 0;
         int posicao;
 
-        Console.WriteLine("Informe a posição: ");
+        Console.WriteLine("Informe o número: ");
         posicao = int.Parse(Console.ReadLine());
 
-        for(int i = 0; i < posicao; i++)
+        int[] vetor = new int[posicao];
+
+        vetor[0] = 1;
+
+
+        if (posicao == 1)
         {
-            Console.Write(ant + prox + " ");
-            
-            prox = ant + prox;
-            ant = prox - ant;
+            Console.WriteLine(vetor[0]);
+        }
+        else if (posicao == 2)
+        {
+            vetor[1] = 1;
+            Console.Write(vetor[0] + ", " + vetor[1]);
+        }
+        else
+        {
+            vetor[1] = 1;
+
+            for (int i = 2; i < posicao; i++)
+            {
+                vetor[i] = vetor[i - 1] + vetor[i - 2];
+            }
+            for (int i = 0; i < vetor.Length; i++)
+            {
+                Console.Write(vetor[i] + ", ");
+            }
 
         }
+
+
+
+
 
     }
 }
